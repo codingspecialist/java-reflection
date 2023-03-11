a1fec5bedcc34c13c789e908e259200ba36f2a88
metacoding <getinthere@naver.com>
2023-03-11 13:56:12 +0900

    자바리í플렉션

---
+++ src/ex01/UserController.java
@@ -0,0 +1,11 @@
+package ex01;
+
+public class UserController {
+    public void login(){
+        System.out.println("login() 호출됨");
+    }
+
+    public void join(){
+        System.out.println("join() 호출됨");
+    }
+}
---
+++ src/ex02/UserController.java
@@ -0,0 +1,13 @@
+package ex02;
+
+public class UserController {
+    @RequestMapping(uri = "/login")
+    public void login(){
+        System.out.println("login() 호출됨");
+    }
+
+    @RequestMapping(uri = "/join")
+    public void join(){
+        System.out.println("join() 호출됨");
+    }
+}
