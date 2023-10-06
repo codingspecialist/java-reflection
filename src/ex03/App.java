dcb41c7a8b2dffa2cd9f94257604dbf5fd9f0907
metacoding <getinthere@naver.com>
2023-10-06 22:51:49 +0900

    스캔수정

--- src/ex03/App.java
+++ src/ex03/App.java
@@ -10,41 +10,47 @@ import java.util.Set;
 
 public class App {
 
-    public static Set<Class> componentScan(String pkg) throws Exception {
+    public static Set<Object> componentScan(String pkg) throws Exception {
+        System.out.println("-------------------- 컴포넌트 스캔 start");
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Set<Class> classes = new HashSet<>();
+        Set<Object> instances = new HashSet<>();
 
         URL packageUrl = classLoader.getResource(pkg);
         File packageDirectory = new File(packageUrl.toURI());
         for (File file : packageDirectory.listFiles()) {
             if (file.getName().endsWith(".class")) {
                 String className = pkg + "." + file.getName().replace(".class", "");
-                //System.out.println(className);
+                System.out.println(className);
                 Class cls = Class.forName(className);
-                classes.add(cls);
-            }
-        }
-        return classes;
-    }
-
-    public static void findUri(Set<Class> classes, String uri) throws Exception {
-        boolean isFind = false;
-        for (Class cls : classes) {
-            if (cls.isAnnotationPresent(Controller.class)) {
-                Object instance = cls.newInstance();
-                Method[] methods = cls.getDeclaredMethods();
-
-                for (Method mt : methods) {
-                    Annotation anno = mt.getDeclaredAnnotation(RequestMapping.class);
-                    RequestMapping rm = (RequestMapping) anno;
-                    if (rm.uri().equals(uri)) {
-                        isFind = true;
-                        mt.invoke(instance);
-                    }
+                if (cls.isAnnotationPresent(Controller.class)) {
+                    System.out.println("스캔완료 : "+cls);
+                    Object instance = cls.newInstance();
+                    instances.add(instance);
                 }
             }
         }
-        if(isFind == false){
+        System.out.println("-------------------- 컴포넌트 스캔 end");
+        return instances;
+    }
+
+    public static void findUri(Set<Object> instances, String uri) throws Exception {
+        boolean isFind = false;
+        for (Object instance : instances) {
+            Class cls = instance.getClass();
+
+            Method[] methods = cls.getDeclaredMethods();
+
+            for (Method mt : methods) {
+                Annotation anno = mt.getDeclaredAnnotation(RequestMapping.class);
+                RequestMapping rm = (RequestMapping) anno;
+                if (rm.uri().equals(uri)) {
+                    isFind = true;
+                    mt.invoke(instance);
+                }
+            }
+
+        }
+        if (isFind == false) {
             System.out.println("404 Not Found");
         }
     }
@@ -53,8 +59,8 @@ public class App {
         Scanner sc = new Scanner(System.in);
         String uri = sc.nextLine();
 
-        Set<Class> classes = componentScan("ex03");
-        findUri(classes, uri);
+        Set<Object> instances = componentScan("ex03");
+        findUri(instances, uri);
 
     }
 
